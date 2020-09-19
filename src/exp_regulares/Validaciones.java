df7ac8bef0ed1caeae3d7664919deef309cce923
jossrc <kuro.rc10@gmail.com>
2020-09-19 15:30:59 -0500

    Realizando las validaciones con expresiones regulares

--- src/exp_regulares/Validaciones.java
+++ src/exp_regulares/Validaciones.java
@@ -1,19 +1,22 @@
 package exp_regulares;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Validaciones extends JFrame {
+public class Validaciones extends JFrame implements ActionListener {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JLabel lblDni;
 	private JLabel lblApellido;
@@ -118,8 +121,105 @@ public class Validaciones extends JFrame {
 		txtCorreo.setColumns(10);
 		
 		btnVerificar = new JButton("Verificar");
+		btnVerificar.addActionListener(this);
 		btnVerificar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnVerificar.setBounds(327, 23, 126, 37);
 		contentPane.add(btnVerificar);
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnVerificar) {
+			actionPerformedBtnVerificar(arg0);
+		}
+	}
+	protected void actionPerformedBtnVerificar(ActionEvent arg0) {
+		
+		String dni = leerDNI();
+		String apellido;
+		String codigo;
+		double precio;
+		String telefono;
+		String correo;
+		
+		
+		if (dni != null) {
+			apellido = leerApellido();
+			if (apellido != null) {
+				precio = leerPrecio();
+				if (precio != -1) {					
+					codigo = leerCodigo();					
+					if (codigo != null) {
+						telefono = leerTelefono();
+						if(telefono != null) {
+							correo = leerCorreo();
+							if (correo != null) {
+								JOptionPane.showMessageDialog(this, "Datos ingresados correctamente");
+							}
+						}
+					}
+				}
+			}
+		} else {
+			return;
+		}
+		
+	}
+	
+	String leerDNI() {
+		
+		if (!txtDNI.getText().trim().matches("\\d{8}")) {
+			JOptionPane.showMessageDialog(this, "DNI inválido");
+			return null;
+		}
+		
+		return txtDNI.getText().trim();
+	}
+	
+	String leerApellido() {
+		
+		if (!txtApellido.getText().trim().matches("[A-Z]{3,30}")) {
+			JOptionPane.showMessageDialog(this, "Apellido inválido");
+			return null;
+		}
+		
+		return txtApellido.getText().trim();
+	}
+	
+	double leerPrecio() {
+		
+		if (!txtPrecio.getText().trim().matches("[0-9]+([.][0-9]{1,2})?")) {
+			JOptionPane.showMessageDialog(this, "Precio inválido");
+			return -1;
+		}
+		
+		return Double.parseDouble(txtPrecio.getText().trim());
+	}
+	
+	String leerCodigo() {
+		
+		if (!txtCodigo.getText().trim().matches("i[1-8]{8}")) {
+			JOptionPane.showMessageDialog(this, "Código inválido");
+			return null;
+		}
+		
+		return txtCodigo.getText().trim();
+	}
+	
+	String leerTelefono() {
+		if(!txtTelefono.getText().trim().matches("[0-9]{3}[-][0-9]{4}")) {
+			JOptionPane.showMessageDialog(this, "Teléfono inválido");
+			return null;
+		}
+			
+		return txtTelefono.getText().trim();
+	}
+	
+	String leerCorreo() {
+		
+		if (!txtCorreo.getText().trim().matches("[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})")) {
+			JOptionPane.showMessageDialog(null, "Correo no válido");
+			return null;
+		}
+		
+		return txtCorreo.getText().trim();
 	}
 }
